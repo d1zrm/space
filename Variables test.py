98c1028038c2aeb2c51165e5b15893d07fe3ae86
d1zrm <150273817+d1zrm@users.noreply.github.com>
2024-03-04 13:33:16 -0500

    Update and rename glizzy.py to Variables test.py

---
+++ Variables test.py
@@ -0,0 +1,18 @@
+#glizzy = "chicken"
+#name = " Jakupi"
+#Full_Sentance = (glizzy+name)
+#print("Nice "+Full_Sentance)
+#print(type(glizzy))
+
+#Age = 21
+#Age += 1
+#print("Your age is "+str(Age))
+#print(type(Age))
+
+#height = 250.5
+#print("Your height is: "+str(height)+"cm")
+#print(type(height))
+
+#Human = False
+#print("Are you a human: "+str(Human))
+#print(type(Human))
